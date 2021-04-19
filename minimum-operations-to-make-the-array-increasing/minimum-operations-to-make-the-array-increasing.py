8323658722e8a8e8c5780ba61a196f74c61a58bb
ku-nal <48153221+ku-nal@users.noreply.github.com>
2021-04-19 23:12:09 +0530

    Time: 96 ms (100.00%), Space: 14.1 MB (100.00%) - LeetHub

---
+++ minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def minOperations(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ans=0
+        for i in range(1,len(nums)):
+            if nums[i]<=nums[i-1]:
+                ans+=nums[i-1]-nums[i]+1
+                nums[i]=nums[i-1]+1
+        return ans
