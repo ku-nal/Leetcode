beaad1e7e47ceffd1a0884ee54e21936f3349d6a
Kunal Makwana <48153221+ku-nal@users.noreply.github.com>
2021-10-09 19:54:16 +0530

    Time: 49 ms (8.37%), Space: 13.7 MB (12.65%) - LeetHub

---
+++ valid-parentheses/valid-parentheses.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack=[]
+        
+        for i in range(len(s)):
+            if s[i]=="{" or s[i]=="[" or s[i]=="(":
+                stack.append(s[i])
+            else:
+                if len(stack)==0:
+                    return False
+                if s[i]=="}":
+                    if stack[-1]!="{":
+                        return False
+                    stack.pop()
+                if s[i]=="]":
+                    if stack[-1]!="[":
+                        return False
+                    stack.pop()
+                if s[i]==")":
+                    if stack[-1]!="(":
+                        return False
+                    stack.pop()
+        if len(stack)==0:return True
+        return False
+                
