969286206acfc76bbbce1feabc317c4b4d77ef9f
Kunal Makwana <48153221+ku-nal@users.noreply.github.com>
2021-11-26 23:35:27 +0530

    Time: 206 ms (20.79%), Space: 13.6 MB (98.54%) - LeetHub

--- is-graph-bipartite/is-graph-bipartite.py
+++ is-graph-bipartite/is-graph-bipartite.py
@@ -4,28 +4,25 @@ class Solution(object):
         :type graph: List[List[int]]
         :rtype: bool
         """
-        visited=[-1]*len(graph)
-        def isbipartite(src,graph,visited):
-            queue=[]
-            queue.append([src,0])
-            while len(queue)>0:
-                rem=queue.pop(0)
-                if visited[rem[0]]!=-1:
-                    if visited[rem[0]]!=rem[1]:
-                        return False
-                else:
-                    visited[rem[0]]=rem[1]
-                for i in graph[rem[0]]:
-                    if visited[i]==-1:
-                        queue.append([i,rem[1]+1])
-            return True
-            
-        for i in range(len(graph)):
-            if visited[i]==-1:
-                boo=isbipartite(i,graph,visited)
-                if not boo:
-                    return False
-                
-        return True
-                
+        color=[-1]*len(graph)
         
+        def bfs(ele):
+            color[ele]=1
+            q=[ele]
+            
+            while len(q):
+                p=q.pop(0)
+                
+                for neigh in graph[p]:
+                    if color[neigh]==-1:
+                        color[neigh]=1-color[p]
+                        q.append(neigh)
+                    elif color[neigh]==color[p]:
+                        return False
+            
+            return True
+        
+        for i in range(len(graph)):
+            if color[i]==-1:
+                if not bfs(i):return False
+        return True
